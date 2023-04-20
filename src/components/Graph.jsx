3c7bc5487530e8b549866dd11e48bb524ef6acbb
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-04-20 08:54:55 +0300

    Added randomize function

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -5,17 +5,31 @@ import {
   CSS2DRenderer,
   CSS2DObject,
 } from '//unpkg.com/three/examples/jsm/renderers/CSS2DRenderer.js';
+import { useEffect, useState } from 'react';
 
 const Graph = () => {
   const extraRenderers = [new CSS2DRenderer()];
+  const [info, setInfo] = useState(Data);
+  const randomize = () => {
+    const newnodes = info.nodes.filter(
+      (item) => (item.value = Math.floor(Math.random() * (100 - 1 + 1) + 1))
+    );
+    const newlinks = info.links.filter(
+      (item) => (item.size = Math.floor(Math.random() * (10) + 1))
+    );
+
+    setInfo({ nodes: newnodes, links: newlinks });
+  };
+
   return (
     <div>
       <ForceGraph3D
         width={700}
         height={500}
-        graphData={Data}
+        graphData={info}
         linkWidth={0.3}
         extraRenderers={extraRenderers}
+        nodeLabel={'value'}
         nodeColor={(node) => {
           return node.color;
         }}
@@ -23,7 +37,9 @@ const Graph = () => {
         nodeResolution={15}
         nodeThreeObject={(node) => {
           const nodeEl = document.createElement('div');
-          nodeEl.textContent = node.val;
+          console.log(node);
+          nodeEl.textContent = node.value;
+
           return new CSS2DObject(nodeEl);
         }}
         nodeThreeObjectExtend={true}
@@ -44,6 +60,7 @@ const Graph = () => {
           Object.assign(sprite.position, middlePos);
         }}
       />
+      <button onClick={() => randomize()}> Randomize</button>
     </div>
   );
 };
